7497e0c059c47f3e221396296ffd19717f6c749b
Petter Wigle <petter@wigle.se>
2011-10-06 10:55:17 +0200

    Storing images and files on S3 when in production configuration

--- app/models/content_file.rb
+++ app/models/content_file.rb
@@ -1,5 +1,11 @@
 class ContentFile < Asset
   has_attached_file :attachment,
     :url => "/assets/content_files/:id/:basename.:extension",
-    :path => ":rails_root/public/assets/content_files/:id/:basename.:extension"
+    :path => Rails.env == 'production' ? ":rails_root/public/assets/content_files/:id/:basename.:extension" : "assets/content_files/:id/:basename.:extension",
+    :storage => Rails.env == 'production' ? 's3' : 'filesystem',
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
+    :bucket => ENV['S3_BUCKET']
 end
--- app/models/content_image.rb
+++ app/models/content_image.rb
@@ -3,5 +3,11 @@ class ContentImage < Image
     :styles => {:mini => '48x48#', :large => '600x600>'},
     :default_style => :large,
     :url => "/assets/content_images/:id/:style/:basename.:extension",
-    :path => ":rails_root/public/assets/content_images/:id/:style/:basename.:extension"
+    :path => Rails.env == 'production' ? ":rails_root/public/assets/content_images/:id/:basename.:extension" : "assets/content_images/:id/:basename.:extension",
+    :storage => Rails.env == 'production' ? 's3' : 'filesystem',
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
+    :bucket => ENV['S3_BUCKET']
 end
